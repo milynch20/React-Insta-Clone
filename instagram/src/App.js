92882994b908ce80738e880f3dd618a08411f565
Malcolm Lynch <milynch1320@gmail.com>
2019-03-27 22:09:47 -0500

    pushing new work, unfinished

---
+++ instagram/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './components/SearchBar/SearchBar'
+
+class App extends Component {
+  render() {
+    return (
+      <SearchBar />
+    );
+  }
+}
+
+export default App;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function SearchBar() {
+    return (
+        <div>
+            <input type="text" placeholder="Search"></input>
+        </div>
+    );
+}
+
+export default SearchBar;
